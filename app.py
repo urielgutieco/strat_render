984d00b95a99702047c996cfee0dc9024f92eb64
urielgutierrezn <uriel.gutierrenz@gmail.com>
2025-12-24 03:00:52 -0700

    desc

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ os.makedirs(TMP_DIR, exist_ok=True)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     is_active = db.Column(db.Boolean, default=True) 
     is_admin = db.Column(db.Boolean, default=False)
 
